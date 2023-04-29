b594f43afaf7148d2b3197e6ebe85869fbf66dbe
rhnnnn <raihanfathi6265@gmail.com>
2023-04-29 14:01:47 +0700

    fix a bit done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
     <link rel="stylesheet" href="{{ asset('css/register.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>a register site you dumbass</title>
+    <title>Register</title>
 </head>
 <body>
     <div class="container">
