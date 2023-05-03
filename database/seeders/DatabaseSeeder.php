75555f72664587766a2ba80b240595e180bb1177
rhnnnn <raihanfathi6265@gmail.com>
2023-05-03 13:23:06 +0700

    seeder done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,14 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\KategoriBuscen;
+use App\Models\KategoriGedung;
+use App\Models\KategoriHealth;
+use App\Models\KategoriKuliner;
+use App\Models\KategoriToko;
+use App\Models\KategoriTourism;
+use App\Models\KategoriOffice;
+use App\Models\LevelSekolah;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +20,144 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        KategoriBuscen::create([
+            'Kategori'=>'Business Center'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        KategoriGedung::create([
+            'Kategori'=>'JKS'
+        ]);
+
+        KategoriGedung::create([
+            'Kategori'=>'DBS'
+        ]);
+
+        KategoriGedung::create([
+            'Kategori'=>'DES'
+        ]);
+
+        KategoriHealth::create([
+            'Kategori'=>'Puskesmas'
+        ]);
+
+        KategoriHealth::create([
+            'Kategori'=>'Klinik'
+        ]);
+
+        KategoriHealth::create([
+            'Kategori'=>'Rumah Sakit'
+        ]);
+
+        KategoriHealth::create([
+            'Kategori'=>'Apotek'
+        ]);
+
+        KategoriKuliner::create([
+            'Kategori'=>'Pujasera'
+        ]);
+
+        KategoriKuliner::create([
+            'Kategori'=>'Warung'
+        ]);
+
+        KategoriKuliner::create([
+            'Kategori'=>'Kafe'
+        ]);
+
+        KategoriKuliner::create([
+            'Kategori'=>'Resto'
+        ]);
+
+        KategoriOffice::create([
+            'Kategori'=>'Professional'
+        ]);
+
+        KategoriOffice::create([
+            'Kategori'=>'Property'
+        ]);
+
+        KategoriOffice::create([
+            'Kategori'=>'Banking'
+        ]);
+
+        KategoriOffice::create([
+            'Kategori'=>'Finance'
+        ]);
+
+        KategoriOffice::create([
+            'Kategori'=>'Logistic'
+        ]);
+
+        KategoriOffice::create([
+            'Kategori'=>'Mining'
+        ]);
+
+        KategoriOffice::create([
+            'Kategori'=>'Energy'
+        ]);
+
+        KategoriOffice::create([
+            'Kategori'=>'Transport'
+        ]);
+
+        KategoriToko::create([
+            'Kategori'=>'Sport & Music'
+        ]);
+
+        KategoriToko::create([
+            'Kategori'=>'Supermarket'
+        ]);
+
+        KategoriToko::create([
+            'Kategori'=>'Minimarket'
+        ]);
+
+        KategoriToko::create([
+            'Kategori'=>'Aksesoris'
+        ]);
+
+        KategoriToko::create([
+            'Kategori'=>'Fashion'
+        ]);
+
+        KategoriToko::create([
+            'Kategori'=>'Electronic & Gadget'
+        ]);
+
+        KategoriToko::create([
+            'Kategori'=>'Mall'
+        ]);
+
+        KategoriTourism::create([
+            'Kategori'=>'Hotel'
+        ]);
+
+        KategoriTourism::create([
+            'Kategori'=>'Travel'
+        ]);
+
+        KategoriTourism::create([
+            'Kategori'=>'Stay House'
+        ]);
+
+        KategoriTourism::create([
+            'Kategori'=>'Kos-kosan'
+        ]);
+
+        LevelSekolah::create([
+            'LEVEL'=>'TK'
+        ]);
+
+        LevelSekolah::create([
+            'LEVEL'=>'SD/MI'
+        ]);
+
+        LevelSekolah::create([
+            'LEVEL'=>'SMP/MTs'
+        ]);
+
+        LevelSekolah::create([
+            'LEVEL'=>'SMA/MA/SMK/MAK'
+        ]);
     }
 }
